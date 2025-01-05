e7129c5e8b49f1ce6484d20b77049faae72d02fa
gosidul <128514950+gosidul@users.noreply.github.com>
2025-01-05 23:33:50 +0100

    Solved some problems, still not working

--- decoder.py
+++ decoder.py
@@ -16,24 +16,84 @@ def load_data_from_file(self, fileName):
                     raise TypeError("The file you provided is not of .bin type")
     return data
 
-class RootNode:
+class Node:
+    def show_subtree(self):
+        indention = ''
+        description = ''
+        description += str(self)
+        if self.link1 is not None and self.link0 is not None:
+            description += ':'
+            child_number = 0
+            children = [self.link0, self.link1]
+            for child in children:
+                description += '\n'
+                description += indention
+                if len(children) - 1 == child_number:
+                    description += '\u2514\u2500> '
+                else:
+                    description += '\u251c\u2500> '
+                if type(child) is InternalNode:
+                    if len(children) - 1 == child_number:
+                        indention += '    '
+                    else:
+                        indention += '\u2502   '
+                    description += child.show_subtree(indention)
+                    indention = indention[:-4]
+                else:
+                    description += str(child)
+                child_number += 1
+        return description
+
+class RootNode(Node):
     def __init__(self, link0, link1):
         self.link0 = link0
         self.link1 = link1
+        self.weight = 0
         self.number = 0
+    def __str__(self):
+        return 'Root'
+
+    
 
 class InternalNode(RootNode):
-    def __init__(self, link0, link1, parent, weigth):
-        super.__init__(self, link0, link1)
+    def __init__(self, link0, link1, parent, weight):
+        super().__init__(link0, link1)
         self.parent = parent
-        self.weigth = weigth
+        self.weight = weight
+    def __str__(self):
+        return 'N'
 
-class ExternalNode:
-    def __init__(self, parent, weigth, value):
+class ExternalNode(Node):
+    def __init__(self, parent, weight, value):
         self.parent = parent
-        self.weigth = weigth # 0 dla NYT
+        self.weight = weight # 0 dla NYT
         self.value = value # None dla NYT
         self.number = 0
+    def __str__(self):
+        if self.value is None:
+            return 'NYT'
+        return str(self.value)
+
+def swapNodes(node1, node2):
+    n1 = node1.number
+    node1.number = node2.number
+    node2.number = n1
+    p1 = node1.parent
+    p2 = node2.parent
+
+    if p1.link0 == node1:
+        p1.link0 = node2
+    elif p1.link1 == node1:
+        p1.link1 = node2
+
+    if p2.link0 == node2:
+        p2.link0 = node1
+    elif p2.link1 == node2:
+        p2.link1 = node1
+
+    node1.parent = p2
+    node2.parent = p1
+
 
 class tree:
     def __init__(self):
@@ -46,69 +106,65 @@ class tree:
                 leaf = node
                 first_occurence = False
         if not first_occurence:
-            current_node = leaf.parent
+            current_node = leaf
             # tu potem wracamy
-            node_with_biggest_num_in_group = max((node for node in self.nodes if node.weight == current_node.weigth), key=lambda node: node.number, default=None)
-            if not current_node.number == node_with_biggest_num_in_group.number:
-                biggest_number = node_with_biggest_num_in_group.number
-                node_with_biggest_num_in_group.number = current_node.number
-                current_node.number = biggest_number
+            node_with_biggest_num_in_group = max((node for node in self.nodes if node.weight == current_node.weight and type(node) != RootNode), key=lambda node: node.number, default=None)
+            if node_with_biggest_num_in_group and not current_node.number == node_with_biggest_num_in_group.number:
+                swapNodes(node_with_biggest_num_in_group, current_node)
+
             current_node.weight = current_node.weight + 1
         else: #pierwsze wystąpienie symbolu
-            old_NYT = (node for node in self.nodes if node.number == 0)
+            for node in self.nodes:
+                if node.number == 0:
+                    old_NYT = node
             new_symbol_node = ExternalNode(None, 1, new_symbol)
             new_symbol_node.number = 1
             new_NYT = ExternalNode(None, 0, None)
             new_NYT.number = 0
             if type(old_NYT) is not RootNode:
                 old_NYT_transformed = InternalNode(new_symbol_node, new_NYT, old_NYT.parent, 1)
+                if old_NYT.parent.link0 == old_NYT:
+                    old_NYT_transformed.parent.link0 = old_NYT_transformed
+                elif old_NYT.parent.link1 == old_NYT:
+                    old_NYT_transformed.parent.link1 = old_NYT_transformed # tu do pomyślenia
             else:
                 old_NYT_transformed = RootNode(new_symbol_node, new_NYT)
+                old_NYT_transformed.weight = 1
             old_NYT_transformed.number = 2
             new_NYT.parent = old_NYT_transformed
             new_symbol_node.parent = old_NYT_transformed
+            # if type(old_NYT) is not RootNode:
+            #     old_NYT_transformed.link0 = new_symbol_node
+            #     old_NYT_transformed.link1 = new_NYT
+            #     new_NYT.parent = old_NYT_transformed
+            #     new_symbol_node.parent = old_NYT_transformed
             self.nodes[:] = [node for node in self.nodes if node.number != 0] # usuń stary NYT
             for node in self.nodes:
                 node.number = node.number + 2
             self.nodes.append(old_NYT_transformed)
             self.nodes.append(new_symbol_node)
             self.nodes.append(new_NYT)
+            self.nodes = sorted(self.nodes, key=lambda node: node.number)
             current_node = old_NYT_transformed
         
         while type(current_node) is not RootNode:
-            node_with_biggest_num_in_group = max((node for node in self.nodes if node.weight == current_node.weigth), key=lambda node: node.number, default=None)
-            if not current_node.number == node_with_biggest_num_in_group.number:
-                biggest_number = node_with_biggest_num_in_group.number
-                node_with_biggest_num_in_group.number = current_node.number
-                current_node.number = biggest_number
+            current_node = current_node.parent
+            node_with_biggest_num_in_group = max((node for node in self.nodes if node.weight == current_node.weight and type(node) != RootNode), key=lambda node: node.number, default=None)
+            if node_with_biggest_num_in_group and not current_node.number == node_with_biggest_num_in_group.number and type(current_node) is not RootNode:
+                swapNodes(node_with_biggest_num_in_group, current_node)
             current_node.weight = current_node.weight + 1
+        self.nodes = sorted(self.nodes, key=lambda node: node.number)
+
+        print(self.nodes[-1].show_subtree())
+    
+    
+
         
 
-
-
-# def get_data(fileName):
-#     with open(fileName, mode='rb') as file:
-#         fileContent = file.read()
-#         data = 0
-#         shift = (len(fileContent) - 1) * 8 
-#         for byte_value in fileContent:
-#             byte_bin_value = bin(byte_value)
-#             data = data + int(byte_value) * (2 ** shift)
-#             shift = shift - 8
-#         return data
-
-# def get_bin_str(data):
-#     binary_data = bin(data)[2:]
-#     zeros = ''
-#     for _ in range((8 - (len(binary_data) % 8)) % 8):
-#         zeros = zeros + '0'
-#     binary_data = zeros + binary_data
-#     return binary_data
-
-# def get_bool_table()
-
-# bin_data = get_bin_str(get_data("compressed.bin"))
-# print(bin_data)
-# # print(bin(get_data("compressed.bin")))
-
-
+symbol_tree = tree()
+symbol_tree.update_tree(62)
+symbol_tree.update_tree(62)
+symbol_tree.update_tree(64)
+symbol_tree.update_tree(64)
+symbol_tree.update_tree(64)
+symbol_tree.update_tree(70)
